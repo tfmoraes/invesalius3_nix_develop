50d9bc0ce055c62c1842c1eca4298dde80f2c9d7
Thiago Franco de Moraes <totonixsame@gmail.com>
2023-01-12 19:34:14 -0300

    Updates

--- flake.nix
+++ flake.nix
@@ -3,22 +3,24 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  inputs.poetry2nix_pkgs.url = "github:nix-community/poetry2nix";
+  inputs.poetry2nix = {
+    url = "github:nix-community/poetry2nix";
+    inputs = {
+      nixpkgs.follows = "nixpkgs";
+      flake-utils.follows = "flake-utils";
+    };
+  };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    poetry2nix_pkgs,
+    poetry2nix,
   }: (flake-utils.lib.eachDefaultSystem (system: let
     pkgs = import nixpkgs {
       inherit system;
       config.allowUnfree = true;
-    };
-
-    poetry2nix = import poetry2nix_pkgs {
-      inherit pkgs;
-      poetry = pkgs.poetry;
+      overlays = [poetry2nix.overlay];
     };
 
     customOverrides = self: super: {
@@ -48,6 +50,54 @@
           ++ (old.buildInputs or []);
         nativeBuildInputs = [self.sip] ++ (old.nativeBuildInputs or []);
       });
+
+      nvidia-cudnn-cu11 = super.nvidia-cudnn-cu11.overridePythonAttrs (
+        attrs: {
+          nativeBuildInputs = attrs.nativeBuildInputs or [] ++ [pkgs.autoPatchelfHook];
+          propagatedBuildInputs =
+            attrs.propagatedBuildInputs
+            or []
+            ++ [
+              self.nvidia-cublas-cu11
+              self.pkgs.cudaPackages.cudnn_8_5_0
+            ];
+          preFixup = ''
+            addAutoPatchelfSearchPath "${self.nvidia-cublas-cu11}/${self.python.sitePackages}/nvidia/cublas/lib"
+          '';
+          postFixup = ''
+            rm -r $out/${self.python.sitePackages}/nvidia/{__pycache__,__init__.py}
+          '';
+        }
+      );
+
+      nvidia-cuda-nvrtc-cu11 = super.nvidia-cuda-nvrtc-cu11.overridePythonAttrs (_: {
+        postFixup = ''
+          rm -r $out/${self.python.sitePackages}/nvidia/{__pycache__,__init__.py}
+        '';
+      });
+
+      torch = super.torch.overridePythonAttrs (attrs: {
+        nativeBuildInputs =
+          attrs.nativeBuildInputs
+          or []
+          ++ [
+            pkgs.autoPatchelfHook
+            pkgs.cudaPackages.autoAddOpenGLRunpathHook
+          ];
+        buildInputs =
+          attrs.buildInputs
+          or []
+          ++ [
+            self.nvidia-cudnn-cu11
+            self.nvidia-cuda-nvrtc-cu11
+            self.nvidia-cuda-runtime-cu11
+          ];
+        postInstall = ''
+          addAutoPatchelfSearchPath "${self.nvidia-cublas-cu11}/${self.python.sitePackages}/nvidia/cublas/lib"
+          addAutoPatchelfSearchPath "${self.nvidia-cudnn-cu11}/${self.python.sitePackages}/nvidia/cudnn/lib"
+          addAutoPatchelfSearchPath "${self.nvidia-cuda-nvrtc-cu11}/${self.python.sitePackages}/nvidia/cuda_nvrtc/lib"
+        '';
+      });
     };
 
     gpu_libs = with pkgs; [
@@ -61,11 +111,11 @@
     ];
 
     my_env =
-      poetry2nix.mkPoetryEnv
+      pkgs.poetry2nix.mkPoetryEnv
       {
         projectDir = ./.;
         preferWheels = true;
-        overrides = [poetry2nix.defaultPoetryOverrides customOverrides];
+        overrides = [customOverrides pkgs.poetry2nix.defaultPoetryOverrides];
         python = pkgs.python310;
       };
   in {
