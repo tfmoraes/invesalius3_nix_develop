047644fdd41bf2ccd5290231f194e15ccaaa2503
Thiago Franco de Moraes <totonixsame@gmail.com>
2023-08-02 19:24:11 -0300

    Updates

--- flake.nix
+++ flake.nix
@@ -11,87 +11,106 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    poetry2nix,
-  }: (flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-      overlays = [poetry2nix.overlay];
-    };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , poetry2nix
+    ,
+    }: (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [ poetry2nix.overlay ];
+      };
 
-    customOverrides = self: super: {
-      scikit-build = super.scikit-build.overridePythonAttrs (
-        old: {
-          buildInputs = [self.wheel] ++ (old.buildInputs or []);
-        }
-      );
-      plaidml = super.plaidml.overridePythonAttrs (old: {
-        pipInstallFlags = "--no-deps";
-      });
+      customOverrides = self: super: {
+        scikit-build = super.scikit-build.overridePythonAttrs (
+          old: {
+            buildInputs = [ self.wheel ] ++ (old.buildInputs or [ ]);
+          }
+        );
+        plaidml = super.plaidml.overridePythonAttrs (old: {
+          pipInstallFlags = "--no-deps";
+        });
 
-      plaidml-keras = super.plaidml-keras.overridePythonAttrs (old: {
-        pipInstallFlags = "--no-deps";
-      });
+        plaidml-keras = super.plaidml-keras.overridePythonAttrs (old: {
+          pipInstallFlags = "--no-deps";
+        });
 
-      pyacvd = super.pyacvd.overridePythonAttrs (old: {
-        buildInputs = [self.cython] ++ (old.buildInputs or []);
-      });
+        pyacvd = super.pyacvd.overridePythonAttrs (old: {
+          buildInputs = [ self.cython ] ++ (old.buildInputs or [ ]);
+        });
 
-      # wxpython = pkgs.python311Packages.wxPython_4_2;
-      wxpython = super.wxpython.overridePythonAttrs (old: {
-        buildInputs =
-          [
-            self.attrdict
-            self.setuptools
-          ]
-          ++ (old.buildInputs or []);
-        nativeBuildInputs = [self.sip] ++ (old.nativeBuildInputs or []);
-      });
+        # wxpython = pkgs.python311Packages.wxPython_4_2;
+        wxpython = super.wxpython.overridePythonAttrs (old: {
+          buildInputs =
+            [
+              self.attrdict
+              self.setuptools
+            ]
+            ++ (old.buildInputs or [ ]);
+          nativeBuildInputs = [ self.sip ] ++ (old.nativeBuildInputs or [ ]);
+        });
 
-      pybind11 = pkgs.python311Packages.pybind11;
-    };
+        pybind11 = pkgs.python311Packages.pybind11;
 
-    gpu_libs = with pkgs; [
-      cudaPackages_11.cudatoolkit
-      cudaPackages_11.cudatoolkit.lib
-      cudaPackages_11.cudnn
-      cudaPackages_11.libcufft
-      cudaPackages_11.libcublas
-      cudaPackages_11.libcurand
-      ocl-icd
-    ];
+        torch = super.torch.overridePythonAttrs
+          (old: {
+            buildInputs = [
+              self.nvidia-cublas-cu11
+              self.nvidia-cuda-cupti-cu11
+              self.nvidia-cuda-nvrtc-cu11
+              self.nvidia-cuda-runtime-cu11
+              self.nvidia-cudnn-cu11
+              self.nvidia-cufft-cu11
+              self.nvidia-curand-cu11
+              self.nvidia-cusolver-cu11
+              self.nvidia-cusparse-cu11
+              self.nvidia-nccl-cu11
+              self.nvidia-nvtx-cu11
+              self.triton
+            ] ++ (old.buildInputs or [ ]);
+          });
+          };
 
-    my_env = (pkgs.poetry2nix.mkPoetryEnv
-      {
-        projectDir = ./.;
-        preferWheels = true;
-        overrides = [customOverrides pkgs.poetry2nix.defaultPoetryOverrides];
-        python = pkgs.python311;
-      })
-    .override {ignoreCollisions = true;};
-  in {
-    devShell = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        poetry
-        my_env
-        gtk3
-        glib
-        gsettings-desktop-schemas
-        clinfo
-        zlib
-        cmake
-      ];
-      # ++ gpu_libs;
-      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-        "/run/opengl-driver"
-        "${my_env}/${my_env.python.sitePackages}/nvidia/cudnn"
-        "${my_env}/${my_env.python.sitePackages}/nvidia/cublas"
-      ];
-    };
-    defaultPackage = my_env;
-  }));
-}
+        gpu_libs = with pkgs; [
+          cudaPackages_11.cudatoolkit
+          cudaPackages_11.cudatoolkit.lib
+          cudaPackages_11.cudnn
+          cudaPackages_11.libcufft
+          cudaPackages_11.libcublas
+          cudaPackages_11.libcurand
+          ocl-icd
+        ];
+
+        my_env = (pkgs.poetry2nix.mkPoetryEnv
+          {
+            projectDir = ./.;
+            preferWheels = true;
+            overrides = [ customOverrides pkgs.poetry2nix.defaultPoetryOverrides ];
+            python = pkgs.python311;
+          }).override { ignoreCollisions = true; };
+        in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            poetry
+            my_env
+            gtk3
+            glib
+            gsettings-desktop-schemas
+            clinfo
+            zlib
+            cmake
+          ];
+          # ++ gpu_libs;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            "/run/opengl-driver"
+            "${my_env}/${my_env.python.sitePackages}/nvidia/cudnn"
+            "${my_env}/${my_env.python.sitePackages}/nvidia/cublas"
+          ];
+        };
+        defaultPackage = my_env;
+      }));
+      }
